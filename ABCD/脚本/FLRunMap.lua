8018c01742e92c9e33390ee607ba1a0e71339130
GLANDTW <99168265+GLANDTW@users.noreply.github.com>
2025-11-07 09:43:23 +0800

    Add files via upload

--- ABCD/脚本/FLRunMap.lua
+++ ABCD/脚本/FLRunMap.lua
@@ -13,15 +13,55 @@ g_shengHuaJob="暴徒"
 --设置使用的武器 支持(盾}箭袋}爪}匕首|法杖|单手剑|细剑|单手斧|单手锤|符文匕首|短杖|战杖|弓|长杖|双手剑|双手斧|双手锤) 如果需要用两样中间用|分开
 SetUseWeapon("双手斧")
 --设置自己用的装备类型 
-SetUseZhuangBeiTypeData("Str|StrDex","衣服")
-SetUseZhuangBeiTypeData("Str|StrInt","头盔")
-SetUseZhuangBeiTypeData("Str|StrInt","手套")
-SetUseZhuangBeiTypeData("Str|StrDex","鞋子")
-SetUseZhuangBeiTypeData("StrInt|StrDex|DexInt","项链")
+SetUseZhuangBeiTypeData("StrDex","衣服")
+SetUseZhuangBeiTypeData("Dex","头盔")
+SetUseZhuangBeiTypeData("Str|StrInt|Int","手套")
+SetUseZhuangBeiTypeData("Str|StrDex|Dex","鞋子")
+SetUseZhuangBeiTypeData("StrInt","项链")
 
-g_attackDis=5			--攻击距离
-g_duobiHpVal=0.2
-g_useMoveSkillType=nil
+
+g_needMinimizeGame=false			--最小化遊戲窗口 true为最小化 false或nil为不最小化
+g_imBoss=false					--如果是仓库号要为true 挂机号为false
+g_attackDis=10					--攻击距离
+g_yiJieTimeOut=15*60			--异界地图内的超时时间 单位为秒 如果未设置就会用g_timeOut *是乘号 这里为15乘以60=15分钟
+g_timeOut=25*60					--在一个地图呆的时间超过设置的 就重新开图重新 单位为秒
+g_addTianFu=true				--自动按设定好的配置加天赋 true为加 nil或false为不加
+g_shengJiBaoShi=true			--自动升级宝石 true为升级 nil或false为不升级
+g_sellSkillGem=false				--自动卖超过40品质的技能宝石
+g_notHuanYaoLv=90				--大于等于多少级只捡蓝色以上药 
+g_needAutoChangeEquip=false		--开启自动换装 true为换 nil或false为不换
+g_needAutoSetBaoShi=false		--开启自动换宝石 true为换 nil或false为不换
+g_bossGiveZhuangBei=false		--交易时仓库号给予缺少的高级换装中的装备 true为给 false为不给 是对仓库号设置的
+g_xiaoHaoGetZhuangBei=false		--挂机号向发装仓库号申请领高级换装中的装备 true为申请 false为不申请 
+
+
+g_yiJieLv=70				--够了多少级才去刷异界
+g_checkSellMapCnt=40		--地图超过这个数量才会检测出售地图
+g_destroyNoUseMap=false		--销毁上面设置不使用的地图 nil或false为不销毁
+g_mapUseFuHaoLv=75			--大于等于多少级对蓝图使用富豪石 nil为永远不使用
+g_mapUseZengFuLv=nil			--大于等于多少级对蓝图使用增幅石 nil为永远不使用
+g_mapUseDianJingLv=75		--大于等于多少级对白图使用点金石 nil为永远不使用
+g_mapUseTuiBianLv=60		--大于等于多少级对白图使用蜕变石 nil为永远不使用
+g_mapUseJiHuiLv=nil			--大于等于多少级对白图使用机会石 nil为永远不使用
+g_mapUseWaErLv=80			--大于等于多少级对白、蓝地图使用瓦尔宝珠 nil为永远不使用
+g_mapUseDingZiLv=nil			--大于等于多少级使用制图钉 nil为永远不使用
+g_noUseMapLv=0				--不使用、不捡多少阶及以上的地图 nil或0为忽略 设置优先的地图除外
+
+g_openQlkMaxMapTianFuCnt=nil--数值型 满多少天赋后不开奇拉克任务nil为一直开
+g_useMinLvMapTianFuCnt=100--地图天赋满多少点后 就从低阶地图开始取
+SetYiJieShuaTuModeByMapCnt(60,nil,2)--)--根据仓库里的地图数量设置异界刷图模式 可添加多个 他是从地图数量多到少来判断的
+SetOpenMapMasterData(3)
+g_notShuaBaiPao=true
+
+
+SetNoUseMapShuXing("map_monsters_reflect_%_physical_damage")--设置不打Physical反射属性
+SetNoUseMapShuXing("map_monsters_reflect_%_elemental_damage")--设置不打魔法反射属性
+SetNoUseMapShuXing("map_players_no_regeneration_including_es")--无法回复
+SetNoUseMapShuXing("is_blighted_map")--凋落地图
+
+
+g_duobiHpVal=0.3
+g_useMoveSkillType=nil --使用位移
 
 --SetNeedSkillLineData(val,str,invalidLv,pos)--设置换技能宝石数据 
 --val=每块宝石的比重值
@@ -31,15 +71,6 @@ g_useMoveSkillType=nil
 --nType 有效场景 nil为无论何时都有效 0为没在刷异界才有效 1为只在刷异界才有效
 
 
-SetNeedSkillLineData(93,"沉重之擊,nil|殘暴輔助,nil",2,nil,nil)
-SetNeedSkillLineData(94,"裂地之擊,nil|殘暴輔助,nil|機率流血輔助,nil",12,nil,nil)
-SetNeedSkillLineData(95,"灰燼之捷,nil|純淨之捷,nil|撲殺輔助,nil",nil,nil,nil)
-SetNeedSkillLineData(96,"威嚇戰吼,nil|緊急詔令輔助,nil",nil,nil,nil)
-SetNeedSkillLineData(97,"鮮血狂怒,nil|受傷時施放輔助,nil|不朽怒嚎,nil",nil,nil,nil)
-SetNeedSkillLineData(98,"躍擊,nil|快速攻擊輔助,nil|脆弱詛咒,nil",nil,nil,nil)
-SetNeedSkillLineData(99,"大地震擊,nil|殘暴輔助,nil|近身戰輔助,nil|近戰物理傷害輔助,nil|拳霸輔助,nil|機率流血輔助,nil",nil,nil,nil)
-
-
 --添加攻击技能 name=技能名 className=技能类名 noLine=不在直线就能攻击 挑选攻击技能会从上到下寻找，要把厉害的技能加在前面
 AddAttackSkillData("碎地重擊","Earthshatter",false,100,0)--添加攻击技能
 AddAttackSkillData("大地震擊","Sunder",false,50,0)--添加攻击技能
@@ -47,24 +78,23 @@ AddAttackSkillData("裂地之擊","Ground Slam",false,40,0)--添加攻击技能
 AddAttackSkillData("沉重之擊","Heavy Strike",false,30,0)
 AddAttackSkillData("普通攻击","melee")
 
+
 --SetSkillLimitMaxLv(name,className,maxLv)--设置技能宝石最大等级 name=技能宝石物品名 className=技能宝石物品类名 maxLv=限制的最大等级
-SetSkillLimitMaxLv("不朽怒嚎",nil,3)
-SetSkillLimitMaxLv("受傷時施放輔助",nil,1)
-SetSkillLimitMaxLv("脆弱","Metadata/Items/Gems/SkillGemVulnerability",20)
+-- SetSkillLimitMaxLv("鋼筋鐵骨",nil,8)
 
 -- SetYiJieZhaoHuanLingTiData(mapClassName,name,className)--设置异界时召唤的灵体数据 mapClassName=剧情图类名 name=灵体名 className=灵体类名
 
---药瓶摆放
 --SetNeedFlaskData(pos,flask,modsData,minLv)--设置药瓶摆放
-SetNeedFlaskData(1,"生命药剂","熱情洋溢的,FlaskPartialInstantRecovery3|密封之,FlaskBleedCorruptingBloodImmunity1",90)
-SetNeedFlaskData(2,"魔力药剂","耐久的,FlaskEffectNotRemovedOnFullMana1|奧術師之FlaskBuffReducedManaCostWhileHealing4",90)
-SetNeedFlaskData(3,"宝钻药剂","nil")
+SetNeedFlaskData(1,"生命药剂","nil")
+SetNeedFlaskData(2,"生命药剂","nil")
+SetNeedFlaskData(3,"魔力药剂","nil")
 SetNeedFlaskData(4,"紫晶药剂","nil")
 SetNeedFlaskData(5,"水银药剂","nil")
 
 
+
 g_addHpVal=0.7--HP低于多少吃红药
-g_addMpVal=0.2--MP低于多少吃蓝药
+g_addMpVal=0.15--MP低于多少吃蓝药
 
 ---------------------------------------换装设置
 --SetAtuoChangeEquipData(job,pos,name,val,yijie) 计算方式如下 定义过的属性值乘以比重值  哪样装备高就会用哪样
@@ -73,122 +103,42 @@ g_addMpVal=0.2--MP低于多少吃蓝药
 --name 属性名 支持游戏内的装备属性名 还有其他的自定义名(物理伤害、护甲、护盾、闪避、连洞、总洞)
 --val 比重值 支持小数
 --nType 换装类型 0或nil或不填为一直有效 1为跑图时才有效 2为异界时才有效
-SetAtuoChangeEquipData("暗影|野蛮人|决斗者|女巫|游侠|圣堂武僧","戒指|项链|腰带|鞋子|手套|衣服|头盔|箭袋","fire_and_cold_damage_resistance_%",0.25)--火焰与冰霜伤害抗性 %
-SetAtuoChangeEquipData("暗影|野蛮人|决斗者|女巫|游侠|圣堂武僧","戒指|项链|腰带|鞋子|手套|衣服|头盔|箭袋","fire_and_lightning_damage_resistance_%",0.25)--火焰与闪电伤害抗性 %
-SetAtuoChangeEquipData("暗影|野蛮人|决斗者|女巫|游侠|圣堂武僧","戒指|项链|腰带|鞋子|手套|衣服|头盔|箭袋","cold_and_lightning_damage_resistance_%",0.2)--冰霜与闪电伤害抗性 %
-SetAtuoChangeEquipData("暗影|野蛮人|决斗者|女巫|游侠|圣堂武僧","戒指|项链|腰带|鞋子|手套|衣服|头盔|箭袋","base_resist_all_elements_%",0.35)--全元素抗性 %
-SetAtuoChangeEquipData("暗影|野蛮人|决斗者|女巫|游侠|圣堂武僧","戒指|项链|腰带|鞋子|手套|衣服|头盔|箭袋","base_fire_damage_resistance_%",0.3)--基础火焰伤害抗性 %
-SetAtuoChangeEquipData("暗影|野蛮人|决斗者|女巫|游侠|圣堂武僧","戒指|项链|腰带|鞋子|手套|衣服|头盔|箭袋","base_cold_damage_resistance_%",0.3)--基础冰霜伤害抗性 %
-SetAtuoChangeEquipData("暗影|野蛮人|决斗者|女巫|游侠|圣堂武僧","戒指|项链|腰带|鞋子|手套|衣服|头盔|箭袋","base_lightning_damage_resistance_%",0.3)--基础闪电伤害抗性 %
-SetAtuoChangeEquipData("暗影|野蛮人|决斗者|女巫|游侠|圣堂武僧","戒指|项链|腰带|鞋子|手套|衣服|头盔|箭袋","cold_damage_resistance_%",0.3)--冰霜抗性 %
-SetAtuoChangeEquipData("暗影|野蛮人|决斗者|女巫|游侠|圣堂武僧","戒指|项链|腰带|鞋子|手套|衣服|头盔|箭袋","fire_damage_resistance_%",0.3)--火焰抗性 %
-SetAtuoChangeEquipData("暗影|野蛮人|决斗者|女巫|游侠|圣堂武僧","戒指|项链|腰带|鞋子|手套|衣服|头盔|箭袋","lightning_damage_resistance_%",0.3)--闪电抗性 %
-SetAtuoChangeEquipData("暗影|野蛮人|决斗者|女巫|游侠|圣堂武僧","戒指|项链|腰带|鞋子|手套|衣服|头盔|箭袋","base_movement_velocity_+%",0.3)--基础移动速度 +%
-SetAtuoChangeEquipData("暗影|野蛮人|决斗者|女巫|游侠|圣堂武僧","戒指|项链|腰带|鞋子|手套|衣服|头盔|箭袋","base_maximum_life",0.3)--生命
-SetAtuoChangeEquipData("暗影|野蛮人|决斗者|女巫|游侠|圣堂武僧","戒指|项链|腰带|鞋子|手套|衣服|头盔|箭袋","movement_velocity_+%",0.3)--移动速度 +%
 
-SetAtuoChangeEquipData("野蛮人","项链","additional_strength",0.2)--
-SetAtuoChangeEquipData("野蛮人","项链","additional_intelligence",0.2)--
-SetAtuoChangeEquipData("野蛮人","项链","additional_strength_and_intelligence",0.3)--
-SetAtuoChangeEquipData("野蛮人","双手斧","local_maximum_added_fire_damage",5)--
-SetAtuoChangeEquipData("野蛮人","双手斧","local_maximum_added_cold_damage",5)--
-SetAtuoChangeEquipData("野蛮人","双手斧","local_maximum_added_lightning_damage",5)--
-SetAtuoChangeEquipData("野蛮人","双手斧","local_maximum_added_physical_damage",5)--每点物理伤害加xx点比重
+--
 
 
---野蛮人
+--萬用換裝--孔色
+
+--SetGaoJiHuanZhuangData("游侠","简易之袍","Metadata/Items/Armours/BodyArmours/BodyInt1","无尽之衣","Tabula Rasa")
+--SetGaoJiHuanZhuangData("游侠","金缕帽","Metadata/Items/Armours/Helmets/HelmetDex1","金缕帽","Goldrim")
+--SetGaoJiHuanZhuangData("游侠","骤雨之弦","Metadata/Items/Weapons/TwoHandWeapons/Bows/Bow2","骤雨之弦","Quill Rain")
 
 
---野蛮人--孔色
-
---衣服
-SetZhengTiColorVarData(2,6,0,0,nil,800,true)
-SetZhengTiColorVarData(2,5,1,0,nil,700,true)
-SetZhengTiColorVarData(2,4,2,0,nil,600,true)
-SetZhengTiColorVarData(2,4,1,1,nil,500,true)
-SetZhengTiColorVarData(2,3,1,0,nil,400,true)
-SetZhengTiColorVarData(2,3,0,1,nil,300,true)
-SetZhengTiColorVarData(2,3,1,1,nil,250,true)
-SetZhengTiColorVarData(2,3,0,0,nil,200,true)
-SetZhengTiColorVarData(2,2,1,0,nil,150,true)
-SetZhengTiColorVarData(2,1,0,0,nil,100)
-SetZhengTiColorVarData(2,0,1,0,nil,60)
-SetZhengTiColorVarData(2,0,0,1,nil,50)
-
---Weapon
-SetZhengTiColorVarData(3,6,0,0,nil,800,true)
-SetZhengTiColorVarData(3,5,1,0,nil,700,true)
-SetZhengTiColorVarData(3,4,2,0,nil,600,true)
-SetZhengTiColorVarData(3,4,1,1,nil,500,true)
-SetZhengTiColorVarData(3,3,1,0,nil,400,true)
-SetZhengTiColorVarData(3,3,0,1,nil,300,true)
-SetZhengTiColorVarData(3,3,1,1,nil,250,true)
-SetZhengTiColorVarData(3,3,0,0,nil,200,true)
-SetZhengTiColorVarData(3,2,1,0,nil,150,true)
-SetZhengTiColorVarData(3,1,0,0,nil,100)
-SetZhengTiColorVarData(3,0,1,0,nil,60)
-SetZhengTiColorVarData(3,0,0,1,nil,50)
-
-
---头盔
-
-SetZhengTiColorVarData(5,3,1,0,nil,500,true)
-SetZhengTiColorVarData(5,3,0,1,nil,400,true)
-SetZhengTiColorVarData(5,3,0,0,nil,300,true)
-SetZhengTiColorVarData(5,2,1,0,nil,200,true)
-SetZhengTiColorVarData(5,1,1,0,nil,100)
-
---手套
-
-SetZhengTiColorVarData(9,4,0,0,nil,700,true)
-SetZhengTiColorVarData(9,3,0,1,nil,600,true)
-SetZhengTiColorVarData(9,3,1,0,nil,500,true)
-SetZhengTiColorVarData(9,2,2,0,nil,400,true)
-SetZhengTiColorVarData(9,2,1,0,nil,300,true)
-SetZhengTiColorVarData(9,2,0,0,nil,200,true)
-SetZhengTiColorVarData(9,1,0,0,nil,100)
-
---鞋子
-
-SetZhengTiColorVarData(10,4,0,0,nil,700,true)
-SetZhengTiColorVarData(10,3,1,0,nil,600,true)
-SetZhengTiColorVarData(10,3,0,1,nil,500,true)
-SetZhengTiColorVarData(10,2,2,0,nil,400,true)
-SetZhengTiColorVarData(10,2,1,0,nil,300,true)
-SetZhengTiColorVarData(10,2,0,0,nil,200,true)
-SetZhengTiColorVarData(10,1,0,0,nil,100)
-
-
-
-AddShengJiZhuangBeiGoodsData("点金石","Metadata/Items/Currency/CurrencyUpgradeToRare",0,50,0)--主线生效 超过50级就用点金点白色装备
-AddShengJiZhuangBeiGoodsData("富豪石","Metadata/Items/Currency/CurrencyUpgradeMagicToRare",1,50,0)--主线生效 超过50级就用富豪点蓝色装备
+--AddShengJiZhuangBeiGoodsData("点金石","Metadata/Items/Currency/CurrencyUpgradeToRare",0,30,0)--主线生效 超过50级就用点金点白色装备
+--AddShengJiZhuangBeiGoodsData("富豪石","Metadata/Items/Currency/CurrencyUpgradeMagicToRare",1,30,0)--主线生效 超过50级就用富豪点蓝色装备
 
 --掉落率+稀有率 异界时才有效
 --SetAtuoChangeEquipData("暗影|野蛮人|决斗者|女巫|游侠|圣堂武僧","戒指|项链|腰带|鞋子|手套|衣服|头盔|法杖|盾","base_item_found_rarity_+%",0.5,2)--基礎物品掉落品質 +%
 --SetAtuoChangeEquipData("暗影|野蛮人|决斗者|女巫|游侠|圣堂武僧","戒指|项链|腰带|鞋子|手套|衣服|头盔|法杖|盾","base_item_found_quantity_+%",1.5,2)--物品掉落數量增加 %
 
 --高级换装设置
-SetGaoJiHuanZhuangData("游侠","簡易之袍","Metadata/Items/Armours/BodyArmours/BodyInt1","無盡之衣","Tabula Rasa",nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,5000)
-SetGaoJiHuanZhuangData("游侠","皮帽","Metadata/Items/Armours/Helmets/HelmetDex1","金縷帽","Goldrim",nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,5000)
+
 
 --SetTaskShengJiData(taskClassName,taskIndex,shengjiMapClassName,needLv)--设置跑图时升级 taskClassName=任务类名 taskIndex=任务索引 nil为忽略，只匹配任务类名 shengjiMapClassName=要刷的地图类名 needLv=升级到多少级为止
-SetTaskShengJiData("a5q7",nil,"1_5_5",46,true)--做 到奇塔弗的受难 的时候刷纯净圣地到50级再进行下一任务
-SetTaskShengJiData("a9q3",nil,"2_9_1",68,true)--做 到風暴飛刃 的时候刷滲血水道到68级再进行下一任务
+SetTaskShengJiData("a5q7",nil,"1_5_5",44,true)--做 到奇塔弗的受难 的时候刷纯净圣地到50级再进行下一任务
+SetTaskShengJiData("a9q3",nil,"2_9_1",63,true)--做 到風暴飛刃 的时候刷滲血水道到68级再进行下一任务
 
 SetTaskMiGongData("a8q7",nil,1)--大于等于a8q7级 做迷宫1
 SetTaskMiGongData("a10q1",nil,2)--大于等于a10q1 做迷宫2
-SetTaskMiGongDataByLv(76,3)--大于等于80级 做迷宫3
+SetTaskMiGongDataByLv(81,3)--大于等于80级 做迷宫3
 
-SetChangeLimitCnt("武器|衣服|头盔|手套|鞋子",2)
-SetChangeLimitCnt("腰带",3)
-SetChangeLimitCnt("项链|戒指",3)
+SetChangeLimitCnt("武器|衣服|头盔|手套|鞋子",0)
+SetChangeLimitCnt("腰带",0)
+SetChangeLimitCnt("项链|戒指",0)
 
 
 --SetNeedAddTianFu(tfStr)--设置天赋加点 
 
-
 SetNeedAddTianFu("野蛮人=melee_damage687-近戰傷害和生命|melee_damage707-近戰傷害|melee_physical_damage1593-近戰物理傷害|melee_physical_damage1592-近戰物理傷害|melee_physical_damage1591-近戰物理傷害|strong_arm118-為戰而生|accuracy1144-命中|versatility1146-俊敏|weapon_damage1883-近戰傷害|two_handed_weapon_damage603-雙手武器近戰傷害|butchery641-屠戮|two_handed_weapon_damage12-雙手武器近戰傷害|attack_speed1889-近戰攻擊速度|strength822-力量|life_resistances1898-生命與元素抗性|life_resistances_notable1900-野蠻|life_resistances1899-生命與元素抗性|strength806-力量|strength791-力量|resolute_technique1138-堅毅之心|rage2100-擊中時獲得盛怒|rage2101-擊中時獲得盛怒|rage_notable2104-戰鬥冥想|strength787-力量|axe_damage_and_accuracy106-斧的傷害|axe_damage_and_attack_speed97-斧的傷害和攻擊速度|axe_damage_and_attack_speed98-斧的傷害和攻擊速度|axe_damage521-斧的傷害和攻擊速度|timberwolf105-屠殺 |strength781-力量|strength775-力量|strength793-力量|fortify_2387-最大護體|fortify_2386-最大護體|fortify_2385-最大護體|fortify_notable_2388-堅貞不渝|mastery_fortify8-護體專精-28469|strength794-力量|strength802-力量|two_handed_damage642-雙手武器近戰傷害|two_handed_weapon_attack_speed15-雙手武器近戰傷害和攻擊速度|two_hand_attack_speed1864-雙手武器傷害和攻擊速度|wrecking_ball435-崩雷|mastery_twohand242-雙手武器專精-27095|life599-生命|heart_of_the_gladiator825-戰士之心|life600-生命|troll's_blood203-戰士之血|strength829-力量|dexterity971-敏捷|strength774-力量|two_handed_weapon_damage13-雙手武器近戰傷害和偷取|two_handed_weapon_damage_and_accuracy9-雙手武器近戰傷害和偷取|two_hand_damage_notable1733-武術經驗|intelligence947-智慧|attack_mana_cost_node_2914-攻擊魔力消耗|attack_mana_cost_notable_2916-戰爭之靈|axe_damage_and_accuracy2470-元素抗性|axe_damage_and_accuracy2469-最大火焰抗性|axe_damage_and_accuracy_notable2471-稜彩之膚|strength809-力量|intelligence945-智慧|warcry_cooldown2707-戰吼冷卻時間恢復|warcry_damage2716-戰吼冷卻時間恢復|warcry_damage2715-戰吼冷卻時間恢復|warcry_cooldown_notable2709_-深呼吸|mastery_warcry254-戰吼專精-60034|life_and_armour1902-生命與護甲|life_armour_flask_life_notable1903-戰神|life_and_armour1901-生命與護甲|strength785-力量|warcry_empowered2710-竭盡攻擊傷害|warcry_empowered2712-戰吼速度和竭盡攻擊傷害|warcry_empowered_notable2713-縝密狂怒|life1169-生命|bloodless156-無血之體|life1221-生命|life1195-生命|strength820-力量|armour91-護甲和元素抗性|armour93-護甲和元素抗性|steel_skin1275-堅鋼之靈|strength832-力量|strength1005-力量|dexterity838-敏捷|endurance_charges2423-每顆耐力球護甲|endurance_charges2422-每顆耐力球暈眩持續時間|endurance_charges2424-每顆耐力球範圍效果|endurance_charges_notable2425-不屈之徒|life1213-生命|golem's_blood1088-魔像血統 |")
-
 SetNeedAddTianFu("野蛮人升华=AscendancyBerserker4-物理傷害、擊中獲得盛怒機率|AscendancyBerserker11-屠戮之慾|AscendancyBerserker6-物理傷害、戰吼速度|AscendancyBerserker7-戰爭使者|") 
-
 野蛮人=atlas_boss_adjacent_maps_2-相鄰地圖掉落機率|atlas_path_23_-相鄰地圖掉落機率|atlas_boss_adjacent_maps_1-相鄰地圖掉落機率|atlas_boss_adjacent_maps_8-相鄰地圖掉落機率|atlas_zana_1_11-偵查報告掉落機率|atlas_zana_1_3-偵查報告掉落機率|atlas_zana_1_6-重點偵察|atlas_zana_1_1-偵查報告掉落機率|atlas_zana_1_2-偵查報告掉落機率|atlas_path_96-聖甲蟲掉落率|atlas_map_tier_3_3-雕塑世界|atlas_map_tier_5_2-高階地圖機率|atlas_keystone_fortune_favours_doubled-大膽行動|atlas_map_tier_4_1-高階地圖機率|atlas_map_tier_5_1-高階地圖機率|atlas_path_61-物品數量|atlas_path_44-物品數量|atlas_path_35-物品數量|atlas_path_39-物品數量|atlas_path_34-物品數量|atlas_path_80-物品稀有度|atlas_map_drops_7-物品稀有度|atlas_mod_effect_6-地圖詞綴效果|atlas_keystone_nofragments_1-堅定不移的遠見|atlas_map_tier_3_2-高階地圖機率|atlas_map_tier_3_1-高階地圖機率|atlas_path_38-相鄰地圖掉落機率|atlas_path_21-相鄰地圖掉落機率|atlas_path_27-相鄰地圖掉落機率|atlas_path_28-相鄰地圖掉落機率|atlas_map_tier_1_2_-高階地圖機率|atlas_map_tier_1_1-高階地圖機率|atlas_map_tier_1_3-雕塑山型|atlas_path_37-相鄰地圖掉落機率|atlas_path_30-相鄰地圖掉落機率|atlas_path_18-相鄰地圖掉落機率|atlas_path_17-相鄰地圖掉落機率|atlas_map_tier_2_1-高階地圖機率|atlas_map_tier_2_2-高階地圖機率|atlas_map_tier_2_3-雕塑天空|atlas_path_15-相鄰地圖掉落機率|atlas_path_2-相鄰地圖掉落機率|atlas_path_10-相鄰地圖掉落機率|atlas_path_19-相鄰地圖掉落機率|atlas_path_1-相鄰地圖掉落機率|atlas_path_20-相鄰地圖掉落機率|atlas_path_9-相鄰地圖掉落機率|atlas_path_6-相鄰地圖掉落機率|atlas_path_8-相鄰地圖掉落機率|atlas_path_13-相鄰地圖掉落機率|atlas_path_22-相鄰地圖掉落機率|atlas_path_12-相鄰地圖掉落機率|atlas_path_24_-相鄰地圖掉落機率|atlas_path_16-相鄰地圖掉落機率|atlas_path_25_-相鄰地圖掉落機率|atlas_path_5-相鄰地圖掉落機率|atlas_path_14-相鄰地圖掉落機率|atlas_path_26-相鄰地圖掉落機率|
