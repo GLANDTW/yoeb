8d8e404be3a0afe4142d067b0bf85de2d14ce4a6
GLANDTW <99168265+GLANDTW@users.noreply.github.com>
2025-11-21 13:41:11 +0800

    Add files via upload

--- ABCD/脚本/Mini327.lua
+++ ABCD/脚本/Mini327.lua
@@ -211,7 +211,7 @@ AddNeedBuySkillTime(11,"a11q1")
 
 --剧情主力技能设置
 SetNeedSkillLineData(100,"沉重之擊,nil|殘暴輔助,nil",2,nil,nil)
-SetNeedSkillLineData(110,"火球,nil|附加閃電傷害輔助,nil",5,nil,nil)
+SetNeedSkillLineData(130,"虛空匕首,nil|附加閃電傷害輔助,nil",5,nil,nil)
 SetNeedSkillLineData(120,"裂地之擊,nil|殘暴輔助,nil|機率流血輔助,nil",12,nil,nil)
 SetNeedSkillLineData(130,"神聖火舌圖騰,nil",12,nil,nil)
 SetNeedSkillLineData(77,"召唤愤怒狂灵,Metadata/Items/Gems/SkillGemSummonRagingSpirit|秘术增强(辅),Metadata/Items/Gems/SupportGemArcaneSurge|召唤生物伤害(辅),Metadata/Items/Gems/SupportGemMinionDamage",25,nil,0)
@@ -254,7 +254,7 @@ SetSkillUseWaEr("迅捷","Metadata/Items/Gems/SkillGemHaste","瓦爾．迅捷","Metadat
 AddAttackSkillData("赦免","absolution")
 AddAttackSkillData("召唤愤怒狂灵","summon_raging_spirit")
 AddAttackSkillData("裂地之擊","Ground Slam",false,40,0)--添加攻击技能
-AddAttackSkillData("火球","Fireball",false,35.0)
+AddAttackSkillData("虛空匕首","Ethereal Knives",false,35.0)
 AddAttackSkillData("沉重之擊","Heavy Strike",false,30,0)
 AddAttackSkillData("普通攻击","melee")
 
